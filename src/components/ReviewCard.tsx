44505b1d7ed86ef25795369a595c5ab63331384f
Jonathan Lamontagne Kratz <77954172+epicgamer17@users.noreply.github.com>
2023-11-18 21:55:30 -0500

    Added images to review card

--- src/components/ReviewCard.tsx
+++ src/components/ReviewCard.tsx
@@ -1,5 +1,5 @@
 import { IReview } from '@/lib/types/Review';
-import { formatDate } from '@/lib/utils';
+import { formatDate, getImage } from '@/lib/utils';
 import React from 'react';
 import { Button } from './ui/button';
 import {
@@ -15,6 +15,8 @@ import { StarIcon as StarOutlineIcon } from '@heroicons/react/24/outline';
 import { store } from '@/lib/types';
 import { IItem } from '@/lib/types/Item';
 import { observer } from 'mobx-react-lite';
+import Image from 'next/image'
+import placeholderImage from '../../public/Untitled.png';
 
 interface IReviewCardProps {
   review: IReview;
@@ -43,31 +45,45 @@ const ReviewCard: React.FC<IReviewCardProps> = observer(({ review, item }) => {
 
   return (
     <Card key={review.id} className=''>
-      <CardHeader>
-        <div className='flex flex-row items-center'>
-          {[...Array(review.rating)].map((s, i) => {
-            return <StarSolidIcon key={i} className='w-4 h-4' />;
-          })}
-          {[...Array(10 - review.rating)].map((s, i) => {
-            return <StarOutlineIcon key={i} className='w-4 h-4' />;
-          })}
-          <p className='align-middle'>{review.rating}/10</p>
+      <div className='grid grid-cols-1 md:grid-cols-2'>
+        <div className=''>
+          <CardHeader>
+            <div className='flex flex-row items-center'>
+              {[...Array(review.rating)].map((s, i) => {
+                return <StarSolidIcon key={i} className='w-4 h-4' />;
+              })}
+              {[...Array(10 - review.rating)].map((s, i) => {
+                return <StarOutlineIcon key={i} className='w-4 h-4' />;
+              })}
+              <p className='ml-1 align-middle'>{review.rating}/10</p>
+            </div>
+            <div>
+              <CardTitle>{review.title}</CardTitle>
+              <CardDescription>
+                {formatDate(new Date(review.createdAt))}
+              </CardDescription>
+            </div>
+          </CardHeader>
+          <CardContent>
+            <p className='ml-6 my-2'>{review.comment}</p>
+          </CardContent>
+          <CardFooter>
+            <Button onClick={() => voteHelpful(review.id)}>
+              {`${review.helpfulVotes} people found this review helpful.`}
+            </Button>
+          </CardFooter>
         </div>
-        <div>
-          <CardTitle>{review.title}</CardTitle>
-          <CardDescription>
-            {formatDate(new Date(review.createdAt))}
-          </CardDescription>
+        <div className='p-6'>
+          <div className='relative w-full h-36'>
+            {
+              getImage(review.imageUrl) == placeholderImage ?
+                <></>
+                :
+                <Image src={getImage(review.imageUrl)} fill alt='' className='aspect-auto object-contain' />
+            }
+          </div>
         </div>
-      </CardHeader>
-      <CardContent>
-        <p className='ml-6 my-2'>{review.comment}</p>
-      </CardContent>
-      <CardFooter>
-        <Button onClick={() => voteHelpful(review.id)}>
-          {`${review.helpfulVotes} people found this review helpful.`}
-        </Button>
-      </CardFooter>
+      </div>
     </Card>
   );
 });
